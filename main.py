e87094a88a53ef6bbd6199c542e8c3ed9b486c21
Mohammed Musthafa A <119840348+ceo-zandora@users.noreply.github.com>
2023-01-02 21:15:38 +0530

    Update main.py
    Complete main.py

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import Response
-import os
-from random import randint
-import uuid
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import StreamingResponse
+import uvicorn
+from PIL import Image
+import io
+from app import Removebg
 
 app = FastAPI()
 
-db = []
+def save_image(image):
+    with open('uploaded.png', "wb") as f:
+        f.write(image)
+
+@app.get('/')
+def remove():
+    return 'RemoveBG Powered by Zandora, Kindly visit /removebg to remove background of images.'
+
+@app.post('/removebg')
+async def removebg(file: UploadFile = File(...)):
+    image = await file.read()
+    save_image(image)
+    resp = Removebg('uploaded.png')
+    img = Image.open(resp)
+    img.save('bgremoved.png')
+    file = open('bgremoved.png', mode="rb")
+
+    return StreamingResponse(file, media_type="image/png")
 
 
-@app.post("/images/")
-async def create_upload_file(file: UploadFile = File(...)):
-
-    file.filename = f"{uuid.uuid4()}.jpg"
-    contents = await file.read()  # <-- Important!
-
-    db.append(contents)
-
-    return {"filename": file.filename}
+if __name__=="__main__":
+    uvicorn.run(app, port=5000, host='127.0.0.1')
