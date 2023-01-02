425701fecb65da29d0a03f4df70a4f1a20908c25
Mohammed Musthafa A <119840348+ceo-zandora@users.noreply.github.com>
2023-01-02 15:46:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,16 +18,3 @@ async def create_upload_file(file: UploadFile = File(...)):
     db.append(contents)
 
     return {"filename": file.filename}
-
-
-@app.get("/images/")
-async def read_random_file():
-
-    # get a random file from the image db
-    random_index = randint(0, len(db) - 1)
-
-    # return a response object directly as FileResponse expects a file-like object
-    # and StreamingResponse expects an iterator/generator
-    response = Response(content=db[random_index])
-
-    return response
